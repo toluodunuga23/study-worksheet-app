d4d94342f77eeaddd4eba4f44b6bbcb26564077d
kawe-education <to379@drexel.edu>
2025-03-16 16:09:56 -0400

    initial commit of create-user route

---
+++ app/api/create-user/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req){
+ 
+        const {user}= await req.json();
+
+        const result = await inngest.send({
+            name: "user.create",
+            data:{
+                user: user
+            }
+        })
+
+        return NextResponse.json({result:result})
+
+}
